a488aa276c1e90e7d74c623ad043b0f0fa9f5f60
Sam10F <samueldf7994@gmail.com>
2022-06-18 12:15:34 +0200

    feat: :sparkles: Implements the routes of the app

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,31 @@
 <template>
-    <img alt="Vue logo" src="./assets/images/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <nav><router-link to="/">Exercise 1</router-link> |</nav>
+    <router-view />
 </template>
-
-<script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "./components/HelloWorld.vue";
-
-@Options({
-    components: {
-        HelloWorld,
-    },
-})
-export default class App extends Vue {}
-</script>
 
 <style lang="scss">
 @import url("https://9d0dd7a648345f19af83-877d2ecaf11b88d5e17327c758e17ef6.ssl.cf2.rackcdn.com/museo-sans-1.0.1/css/museo-sans.css");
 
 #app {
     font-family: "MuseoSans";
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
+    background-color: #e5e5e5;
+    height: 100vh;
+    margin: 0;
+    padding: 0;
+}
+
+nav {
+    padding: 30px;
+
+    a {
+        font-weight: bold;
+        color: #2c3e50;
+
+        &.router-link-exact-active {
+            color: #42b983;
+        }
+    }
 }
 </style>
